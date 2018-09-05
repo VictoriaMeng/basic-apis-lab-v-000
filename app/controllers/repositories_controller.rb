2aae88ee174cad68028ab8d403b4d9dd728d7e28
Victoria Meng <nights1ststar@protonmail.com>
2018-09-05 21:23:15 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,7 +5,10 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-    @resp = Faraday.get 'https://api.github.com/v3/search/repositories' do |req|
+    @resp = Faraday.get 'https://api.github.com/search/repositories"' do |req|
+      req.params['client_id'] = '440455c1ca2e5c712ac1'
+      req.params['client_secret'] = '442429ac032f30e60935b4914521738d06e2d0a7'
+      
     end
   end
 end
